c27b3372a0802b05c265e8008c02dc73b16511d5
Piero Donayre <donayrep20@gmail.com>
2026-05-24 18:18:47 -0500

    Avance interfaz de splash y login

--- app/src/main/java/com/cibertec/proyectodam1/SplashActivity.kt
+++ app/src/main/java/com/cibertec/proyectodam1/SplashActivity.kt
@@ -1,17 +1,21 @@
 package com.cibertec.proyectodam1
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class SplashActivity : AppCompatActivity() {
+    private lateinit var ivLogo: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        ivLogo = findViewById<ImageView>(R.id.ivLogo)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
