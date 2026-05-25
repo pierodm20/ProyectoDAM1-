7bc50ca6e32390ca2c628813ba9558cbe4234107
Piero Donayre <donayrep20@gmail.com>
2026-05-24 19:44:56 -0500

    Actualizacion de la logica y agregue nuevas pantallas

--- app/src/main/java/com/cibertec/proyectodam1/MainActivity.kt
+++ app/src/main/java/com/cibertec/proyectodam1/MainActivity.kt
@@ -1,17 +1,53 @@
 package com.cibertec.proyectodam1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvRegistro: TextView
+    private lateinit var tvContraseña: TextView
+    private lateinit var btnIngresar: MaterialButton
+    private lateinit var txtInContraseña: TextInputEditText
+    private lateinit var txtInCorreo: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        tvRegistro = findViewById<TextView>(R.id.tvRegistro)
+        tvContraseña = findViewById<TextView>(R.id.tvContraseña)
+        btnIngresar = findViewById<MaterialButton>(R.id.btnIngresar)
+        txtInContraseña = findViewById<TextInputEditText>(R.id.txtInContraseña)
+        txtInCorreo = findViewById<TextInputEditText>(R.id.txtInCorreo)
+
+        tvRegistro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnIngresar.setOnClickListener {
+            val usuario = txtInCorreo.text.toString()
+            val contraseña = txtInContraseña.text.toString()
+
+            if (usuario.isNotEmpty() && contraseña.isNotEmpty()){
+                if (usuario == "admin" && contraseña == "123"){
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }else{
+                Toast.makeText(this, "Ingrese usuario y o contraseña", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/cibertec/proyectodam1/SplashActivity.kt
+++ app/src/main/java/com/cibertec/proyectodam1/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.cibertec.proyectodam1
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +10,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+    private var timer: CountDownTimer? = null
     private lateinit var ivLogo: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-
         ivLogo = findViewById<ImageView>(R.id.ivLogo)
+
+        ivLogo.animate().rotationBy(360f).setDuration(2000).start()
+
+        timer = object : CountDownTimer(2000, 1000){
+            override fun onFinish() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            override fun onTick(p0: Long) {
+               //Codigo a ejecutarse por cada segundo
+            }
+        }.start()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
