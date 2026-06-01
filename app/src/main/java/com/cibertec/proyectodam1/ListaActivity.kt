96e743b83a4c5fcf91beb389c7aa5e4f1747f4c6
Jose Junior Alonso <junioralonsohuamani@gmail.com>
2026-05-31 23:16:40 -0500

    Agregada ListaActivity, creado activity_lista.xml, actualizado AndroidManifest y HotelesActivity

--- app/src/main/java/com/cibertec/proyectodam1/ListaActivity.kt
+++ app/src/main/java/com/cibertec/proyectodam1/ListaActivity.kt
@@ -3,30 +3,47 @@ package com.cibertec.proyectodam1
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HotelesActivity : AppCompatActivity() {
+class ListaActivity : AppCompatActivity() {
 
     private lateinit var bnvMenu : BottomNavigationView
+    private lateinit var lvListaHoteles: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_hoteles)
+        setContentView(R.layout.activity_lista)
+
+        // 1. Enlazar la lista del layout
+        lvListaHoteles = findViewById(R.id.lvListaHoteles)
+
+        // 2. Datos directos de los hoteles del proyecto para mostrar en la lista
+        val datosHoteles = arrayOf(
+            "Hotel Melia Lima - San Isidro (5 Estrellas)",
+            "Dazzler by Wyndham - Miraflores (Piscina Terraza)",
+            "Sheraton Lima Hotel - Centro de Lima (Gran Confort)",
+            "Hotel Selina Miraflores - Ambiente Juvenil",
+            "Hilton Lima Miraflores - Hospedaje de Lujo"
+        )
+
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, datosHoteles)
+        lvListaHoteles.adapter = adapter
+
 
         bnvMenu = findViewById<BottomNavigationView>(R.id.bnvMenu)
-
-        bnvMenu.selectedItemId = R.id.Hoteles
 
         bnvMenu.setOnItemSelectedListener { item ->
             when(item.itemId){
                 R.id.Hoteles -> {
-                    // SE CONECTA LA LISTA
-                    cambioActivity(ListaActivity::class.java)
+                    cambioActivity(HotelesActivity::class.java)
                     true
                 }
                 R.id.Inicio -> {
@@ -41,7 +58,7 @@ class HotelesActivity : AppCompatActivity() {
             }
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dlHoteles)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dlLista)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
